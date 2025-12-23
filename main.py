f19dafe2f5cbd7129eb98886c3deaeb59f3daa0d
Mohammad Drikvand <mohammaddrikvand217@gmail.com>
2025-12-23 20:13:21 +0330

    Update

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import tkinter as tk
-from ui.he
+from ui.header import create_header
+from ui.library import create_library
+from ui.player import create_player
+from core.config import init_app
+
+def main():
+    root = tk.Tk()
+    init_app(root)
+
+    create_header(root)
+    create_library(root)
+    create_player(root)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
