868c98999c45ae8b56582663921e3e4de2be4154
Mohammad Drikvand <mohammaddrikvand217@gmail.com>
2026-01-06 18:39:15 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,18 +37,18 @@ func_title
 
 mkdir -p /usr/share/Melodpy
 cp setup.sh /usr/share/Melodpy
-cp Melodpy.py /usr/share/Melodpy
+cp melodpy.py /usr/share/Melodpy
 cp -r detect/ lib/ src/ /usr/share/Melodpy
 
 echo "#!/bin/sh" >> /usr/bin/Melodpy
 echo "cd /usr/share/Melodpy" >> /usr/bin/Melodpy
-echo "exec python Melodpy.py \"\$@\"" >> /usr/bin/Melodpy
+echo "exec python melodpy.py \"\$@\"" >> /usr/bin/Melodpy
 cp $path/logo/Melodpy.desktop /usr/share/applications/Melodpy.desktop
 cp $path/logo/Melodpy.png /usr/share/icons/Melodpy.png
-cp Melodpy.py /usr/local/sbin/Melodpy.py
+cp melodpy.py /usr/local/sbin/melodpy.py
 cp -r detect/ lib/ src/ /usr/local/sbin
-chmod +x /usr/local/sbin/Melodpy.py
-chmod +x Melodpy.py
+chmod +x /usr/local/sbin/melodpy.py
+chmod +x melodpy.py
 
 echo -e "[${GREEN}✔${RESET}]Done"
 echo -e "${GREEN} ╔───────────────────────────╗${RESET}"
